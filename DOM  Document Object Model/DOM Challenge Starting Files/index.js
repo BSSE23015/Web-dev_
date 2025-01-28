c5a5aaab4a6108d40acfd998a07e11a23a03554f
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-01-28 11:17:51 +0500

    done

---
+++ DOM  Document Object Model/DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.querySelector("li a").style.color = "red";
+document.querySelector("li.second").style.color = "green";
+document.querySelector("li.third").style.color = "yellow";
+document.getElementById("btn").style.backgroundColor = "yellow";
+// document.querySelector("#btn").classList.add("huge");
