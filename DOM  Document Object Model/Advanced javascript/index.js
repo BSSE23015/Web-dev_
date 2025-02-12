94a2c93148913c9b36e075868f01a4be9a458e74
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-12 11:24:51 +0500

    call back hell

--- DOM  Document Object Model/Advanced javascript/index.js
+++ DOM  Document Object Model/Advanced javascript/index.js
@@ -1,10 +1,28 @@
-function hello() {
-  console.log("hello");
-}
-console.log("one");
-console.log("two");
-setTimeout(() => {
-  console.log("Hello");
-}, 4000);
-console.log("three");
-console.log("four");
+// function hello() {
+//   console.log("hello");
+// }
+// console.log("one");
+// console.log("two");
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 4000);
+// console.log("three");
+// console.log("four");
+function getData(dataID, getNextData) {
+  //2s
+  setTimeout(() => {
+    console.log("data", dataID);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+} 
+//nested callbacks
+//callback hell problem
+getData(1, () => {
+  //calback arrow function
+  getData(2, () => {
+    //callback arrow function
+    getData(3);
+  });
+});
