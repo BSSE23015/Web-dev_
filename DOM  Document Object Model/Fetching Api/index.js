98753d6cf4eebd441f1e8477df6aa1f870f83070
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-16 12:22:08 +0500

    ...

--- DOM  Document Object Model/Fetching Api/index.js
+++ DOM  Document Object Model/Fetching Api/index.js
@@ -1,8 +1,10 @@
 const URL = "https://meowfacts.herokuapp.com/";
-
+const fact = document.querySelector("#fact");
 const getAPI = async () => {
   console.log("Fetching Data....");
   let promise = await fetch(URL);
-  console.log(promise.status);
-  console.log("Data is fetched successfully.");
+  console.log(promise); //JSON format
+  let data = await promise.json(); //second response
+  fact.innerText = data;
+  console.log(data);
 };
