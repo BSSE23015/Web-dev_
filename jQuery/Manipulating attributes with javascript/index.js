14f6bced3fd99a12c0454a3a7632f4edfd14ceff
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-18 21:18:11 +0500

    ////

--- jQuery/Manipulating attributes with javascript/index.js
+++ jQuery/Manipulating attributes with javascript/index.js
@@ -1,1 +1,5 @@
-$("img").
+console.log($("img").attr("src"));
+$("a").attr(
+  "href",
+  "https://www.yahoo.com/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAIiOhvAf8EpPDy3x2V3y1MrsahCaYq8AEkjF7MshS11UiCI2ukRQnwx6j6u0TjQngeik4u_K0ZoIbgiX5e0tblzt5AqJNJPWFiDh7U1rqC49jRRXR6vFFIBTT-WxcfNR8sXWENAo5GjiIMoeKGgOcWf6RdmO3JeZCiagsWCOwPWy"
+);
