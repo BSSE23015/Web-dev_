d7d764948c9b1e02017fdc8924c2372a29f06ace
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-18 21:42:43 +0500

    ./

--- jQuery/Event listeners with jquery/index.js
+++ jQuery/Event listeners with jquery/index.js
@@ -1,10 +1,15 @@
 $("h1").addClass("big-title");
-//in javascript
+//click event in javascript
 // for (var i = 0; i < 5; i++) {
 //   document.querySelectorAll("button")[i].addEventListener("click", () => {
 //     document.querySelector("h1").style.color = "purple";
 //   });
 // }
+//click event listener in jQuery
 $("button").click(() => {
   $("h1").css("color", "purple");
+});
+//keypress event listener in jQuery
+$(document).keypress((event) => {
+  $("h1").text(event.key);
 });
