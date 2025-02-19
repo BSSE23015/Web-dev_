cd906f86b0196a78f41e49b8c794febebdff7d74
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-19 11:03:32 +0500

    ./

---
+++ jQuery/Adding animations/index.js
@@ -0,0 +1,11 @@
+// $("button").on("click", () => {
+//   $("h1").slideUp();//adds slide up animation to h1
+// });
+
+// $("button").on("click", () => {
+//   $("h1").slideToggle(); //toggles btw slideUp and slideDown animation
+// });
+
+// $("button").on("click", () => {
+//   $("h1").animate({ margin: 20 });//adds animation of adding margin 20px
+// });
