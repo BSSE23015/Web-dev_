7d4f6bead08d70b80467311dd1ac55013b350aaa
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-19 12:32:59 +0500

    simon game

--- jQuery/The simon game/Simon Game Challenge Starting Files/index.js
+++ jQuery/The simon game/Simon Game Challenge Starting Files/index.js
@@ -1,6 +1,24 @@
-var array = ["red", "blue", "green", "yellow"];
-
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
 function nextSequence() {
+  // function generate a new random number between 0 and 3, and store it in a variable called randomNumber
   let randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  //a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
+  let randomChosenColour = buttonColours[randomNumber];
+  //Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
+  gamePattern.push(randomChosenColour);
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  $("button").on("click", () => {
+    handler(randomChosenColour);
+  });
+}
+
+function handler(randomChosenColour) {
+  let userChosenColour = randomChosenColour;
 }
