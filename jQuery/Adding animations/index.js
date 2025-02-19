5d482fc687d47ed415154564d299c095f526e14c
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-19 11:09:47 +0500

    animations done

--- jQuery/Adding animations/index.js
+++ jQuery/Adding animations/index.js
@@ -1,11 +1,16 @@
 // $("button").on("click", () => {
 //   $("h1").slideUp();//adds slide up animation to h1
 // });
-
 // $("button").on("click", () => {
 //   $("h1").slideToggle(); //toggles btw slideUp and slideDown animation
 // });
-
 // $("button").on("click", () => {
 //   $("h1").animate({ margin: 20 });//adds animation of adding margin 20px
 // });
+// $("button").on("click", () => {
+//   $("h1").animate({ margin: "30%" }); //if we add percentage we have to add it in commas bcz we can add only numeric values properties like margin padding etc not color bcz it is string property
+// });
+//we can chain animations to add more than one animations
+$("button").on("click", () => {
+  $("h1").slideUp().slideDown().animate({ margin: 30 }); //adds slideup then slidedown and then margin of 30px
+});
