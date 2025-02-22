bad9e304d740f7687d8c859d93dc914c514c76e9
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-02-22 13:18:01 +0500

    native module

--- Node Js/2.2 Native Modules/index.js
+++ Node Js/2.2 Native Modules/index.js
@@ -1,6 +1,12 @@
-const fs = require("fs");
-fs.writeFile("message.txt", "Hello from Hassan.", (err) => {
-  if (err) throw err;
+// const fs = require("fs");
+// fs.writeFile("message.txt", "Hello from Hassan.", (err) => {
+//   if (err) throw err;
 
-  console.log("The file has been saved.");
+//   console.log("The file has been saved.");
+// });
+
+const f2s = require("fs");
+f2s.readFile("./message.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
