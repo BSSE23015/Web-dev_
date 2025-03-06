de5ba0e1007be47c63bc17ed98d66d380295febc
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-03-06 16:13:02 +0500

    postman

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello.</h1>")
+  res.send("<h1>Hello.</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About me</h1>");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>Phone Number</h1><p>+9202022112</p>");
 });
 app.listen(port, () => {
   console.log(`Running Server port ${port}`);
