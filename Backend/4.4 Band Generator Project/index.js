3339348d0dac883cd1a399fc8c38b3f32893079f
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-03-26 17:28:55 +0500

    working on it

--- Backend/4.4 Band Generator Project/index.js
+++ Backend/4.4 Band Generator Project/index.js
@@ -21,20 +21,21 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
   //Step 2 - Make the generate name functionality work
+  let adject_random = Math.floor(Math.random() * adj.length);
+  let noun_random = Math.floor(Math.random() * noun.length);
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
-  var rand_adj_num = Math.floor(Math.random() * 100);
-  var rand_noun_num = Math.floor(Math.random() * 100);
-
   //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
   //scroll down to see the two arrays.
-  var adject = adj[rand_adj_num];
-  var nou = noun[rand_noun_num];
+  let adjective_sel = adj[adject_random];
+  let noun_sel = noun[noun_random];
   //2. Send the index.ejs as a response and add the adjective and noun to the res.render
   //3. Test to make sure that the random words display in the h1 element in index.ejs
-  var name_generated = adject + nou;
-  console.log(name_generated);
-  res.render("index.ejs", { name_generated });
+  console.log(adjective_sel + " " + noun_sel);
+  res.render("index.ejs", {
+    adjective: adjective_sel,
+    noun: noun_sel,
+  });
 });
 
 app.listen(port, () => {
