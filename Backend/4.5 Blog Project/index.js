bd97f6b75ff70a17632f1f2b628694c0a64b78a8
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-03-27 22:08:55 +0500

    working on it

--- Backend/4.5 Blog Project/index.js
+++ Backend/4.5 Blog Project/index.js
@@ -4,11 +4,12 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-
-app.listen(port,()=>{
-    
-})
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
