0517d700116debcb29ed30faf219935337f94859
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-03-28 16:03:37 +0500

    styling

--- Backend/4.5 Blog Project/app.js
+++ Backend/4.5 Blog Project/app.js
@@ -4,11 +4,30 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let blogs = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/blog", (req, res) => {
+  res.render("blog.ejs");
+});
+
+app.get("/newBlog", (req, res) => {
+  res.render("new_blog.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  console.log(title);
+  console.log(description);
+  blogs.push({ title: title, description: description });
+  res.redirect("/blog");
 });
 
 app.listen(port, () => {
