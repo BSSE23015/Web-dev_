0d9b80de1d81d48a0790ba2976450ec116ec76df
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-04-12 16:26:56 +0500

    add backend section

--- Backend/5.6 Secrets Project/index.js
+++ Backend/5.6 Secrets Project/index.js
@@ -11,3 +11,15 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+import express from "express";
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { secret: "", user: "" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port :${port}`);
+});
