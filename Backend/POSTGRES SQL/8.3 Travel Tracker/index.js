a96797e205f2ac2f88e374f1f57af9b97b79833d
Hassan Abid 15 <bsse23015@itu.edu.pk>
2025-06-09 11:40:18 +0500

    Travel tracker part 2 done

--- Backend/POSTGRES SQL/8.3 Travel Tracker/index.js
+++ Backend/POSTGRES SQL/8.3 Travel Tracker/index.js
@@ -14,17 +14,47 @@ const db = new pg.Client({
 
 db.connect();
 
-const result = await db.query("SELECT country_code FROM visited_countries");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+// app.get("/", async (req, res) => {
+//   let countries = [];
+//   result.rows.forEach((country) => {
+//     countries.push(country.country_code);
+//   });
+//   res.render("index.ejs", { countries: countries, total: countries.length });
+// });
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+  return countries;
+}
+
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const country = req.body["country"];
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [country]
+  );
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    console.log("Country added as a visited country");
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
